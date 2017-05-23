d54bd498e2be283f4d0f06ec57032669ca5b0acc
fmiguelgarcia <francisco.miguel.garcia.rodriguez@gmail.com>
2017-05-23 15:01:24 +0200

    Fixing dependecies in test_package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,7 +7,7 @@ username = os.getenv("CONAN_USERNAME", "demo")
 
 class PackageTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "QEAnnotation/0.1.0@%s/%s" % (username, channel)
+    requires = "QEAnnotation/1.0.0@%s/%s" % (username, channel)
     generators = "cmake"
 
     def build(self):
